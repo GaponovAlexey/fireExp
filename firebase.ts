38a789dbb2ea73b85a55d4c0440246bd3a19cb1c
kekelberi <alexei.gaponov@gmail.com>
2022-12-21 21:34:04 -0600

    save

--- firebase.ts
+++ firebase.ts
@@ -1,23 +1,17 @@
-import { initializeApp } from "firebase-admin/app"
-import { getFirestore } from "firebase-admin/firestore"
-require('dotenv').config()
+// import { getFirestore } from 'firebase-admin/firestore';
+// import { initializeApp } from "firebase-admin/app"
 
-//deb
-var log4js = require("log4js")
-var logger = log4js.getLogger()
-logger.level = "debug"
+// require('dotenv').config()
 
-logger.debug("DEBUG")
+// export const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTHOR_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MES_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEAS_ID,
+// }
+// const app = initializeApp(firebaseConfig)
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTHOR_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MES_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEAS_ID,
-}
-const app = initializeApp(firebaseConfig)
-
-export const db = getFirestore(app)
+// export const db = getFirestore(app)
--- server.ts
+++ server.ts
@@ -1,28 +1,39 @@
+import express, { Request, Response } from "express"
 var log4js = require("log4js")
 var logger = log4js.getLogger()
 logger.level = "debug"
 
-import express, { Request, Response } from "express"
-import { db } from "./firebase"
 const app = express()
+
+const admin = require("firebase-admin")
+var serviceAccount = require("./key.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://yoursuccess-e1c07-default-rtdb.firebaseio.com",
+})
+const db = admin.firestore()
 app.use(express.json()) // Для req.body.data
+const user = db.collection("users").get()
+logger.debug("DEBUG2")
+
+// app.use(express.urlencoded({ extended: true }))
+
 const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-
-
-
-  // const docRef = db.collection("users").onSnapshot((sn) => {
-    // return sn.docs.forEach((el) => el.data())
-  // })
-
-  // logger.debug(JSON.stringify(docRef.toString))
-
-  res.json({
-    topic: "test-channel",
-    messages: [{ value: "you" }],
+app.get("/", async (req: Request, res: Response) => {
+  const getUsers = async () => {
+    const res = await user
+    return res.docs.map((e: any) => ({ ...e.data(), id: e.id }))
+  }
+  getUsers().then((e) => {
+    res.json({
+      topic: "test-channel",
+      messages: [{ value: e[0].name }],
+    })
   })
 })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
